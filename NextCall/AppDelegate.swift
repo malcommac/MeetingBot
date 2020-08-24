09d4e003751162c1cd700378b6fedfef35284800
Daniele Margutti <hello@danielemargutti.com>
2020-08-24 21:58:24 +0200

    Empty project structure

---
+++ NextCall/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NextCall
+//
+//  Created by daniele on 24/08/2020.
+//  Copyright © 2020 com.spillover.nextcall. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NextCall/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NextCall
+//
+//  Created by daniele on 24/08/2020.
+//  Copyright © 2020 com.spillover.nextcall. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
